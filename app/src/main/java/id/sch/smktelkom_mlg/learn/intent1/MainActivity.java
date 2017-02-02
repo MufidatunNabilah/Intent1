59bc64282adc192804c71b7f72215b85228cb18b
Mufidatun Nabilah <mufida.nabilah3@gmail.com>
2017-02-02 13:07:12 +0700

    startActivity Part 2

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/MainActivity.java
@@ -1,21 +1,45 @@
 package id.sch.smktelkom_mlg.learn.intent1;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String NAMA = "Nama";
+    public static final String UMUR = "Umur";
+    EditText etNama;
+    EditText etUmur;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("Input");
         setContentView(R.layout.activity_main);
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        etUmur = (EditText) findViewById(R.id.editTextUmur);
+
         findViewById(R.id.buttonHitung).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, HasilActivity.class));
+            public void onClick(View v) {
+                goHasil();
             }
         });
+    }
+
+    private void goHasil() {
+        String name = etNama.getText().toString();
+        String usia = etUmur.getText().toString();
+
+        int umur = usia.isEmpty() ? 0 : Integer.parseInt(usia);
+
+        Intent intent = new Intent(MainActivity.this, HasilActivity.class);
+        intent.putExtra(NAMA, name);
+        intent.putExtra(UMUR, umur);
+
+        startActivity(intent);
+
     }
 }
